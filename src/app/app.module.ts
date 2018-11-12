e2744f7b4873903f92865252fa523dfe433022ec
Alexandr Orlov <alexandr.orlov@thomsonreuters.com>
2018-11-12 13:19:04 +0400

    refactoring

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,41 +1,34 @@
-import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
-import {StoreDevtoolsModule} from '@ngrx/store-devtools';
-import {BrowserModule} from '@angular/platform-browser';
-import {RouterModule} from '@angular/router';
-import {StoreModule} from '@ngrx/store';
-import {NgModule} from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { StoreModule } from '@ngrx/store';
+import { NgModule } from '@angular/core';
 
-import {NotFoundPageComponent, CoursesPageComponent, AddNewPageComponent, LoginPageComponent, EditCoursePageComponent} from './pages';
-import {AppComponent} from './app.component';
-import {
-    BreadcrumbsComponent,
-    CoursesListComponent,
-    CourseItemComponent,
-    ToolPanelComponent,
-    LoadMoreComponent,
-    FooterComponent,
-    HeaderComponent,
-    IconComponent
-} from './components';
+import { AppComponent } from './app.component';
+import * as components from './components';
+import * as pages from './pages';
 
-import {ROUTES} from './app.routes';
-import {reducers} from '../store';
+import { ROUTES } from './app.routes';
+import { reducers } from '../store';
 
 @NgModule({
     declarations: [
-        EditCoursePageComponent,
-        NotFoundPageComponent,
-        CoursesPageComponent,
-        BreadcrumbsComponent,
-        CoursesListComponent,
-        AddNewPageComponent,
-        CourseItemComponent,
-        LoginPageComponent,
-        ToolPanelComponent,
-        LoadMoreComponent,
-        HeaderComponent,
-        FooterComponent,
-        IconComponent,
+        pages.EditCoursePageComponent,
+        pages.NotFoundPageComponent,
+        pages.CoursesPageComponent,
+        pages.AddNewPageComponent,
+        pages.LoginPageComponent,
+
+        components.BreadcrumbsComponent,
+        components.CoursesListComponent,
+        components.CourseItemComponent,
+        components.ToolPanelComponent,
+        components.LoadMoreComponent,
+        components.HeaderComponent,
+        components.FooterComponent,
+        components.IconComponent,
+
         AppComponent
     ],
     imports: [
