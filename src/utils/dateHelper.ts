72dc4ece5110323e72667bbc2194e7dcbc81d2bb
Alexandr Orlov <alexandr.orlov@thomsonreuters.com>
2018-11-14 19:03:46 +0400

    Border bug fixed

--- src/utils/dateHelper.ts
+++ src/utils/dateHelper.ts
@@ -2,7 +2,7 @@ import { ICourseItem } from '../app/models';
 import * as moment from 'moment';
 
 function checkDate(courseCreationDate, freshBorder, futureBorder) {
-    if (moment(courseCreationDate).isBetween(moment(), moment(moment().subtract('days', 14)).toDate())) {
+    if (moment(courseCreationDate).isBetween(moment(moment().subtract('days', 14)).toDate(), moment())) {
         return freshBorder;
     } else if (moment(moment().toDate()).isBefore(moment(courseCreationDate).toDate())) {
         return futureBorder;
@@ -15,7 +15,6 @@ function orderByDateHelper(array: ICourseItem[]) {
         const dateB: any = new Date(b.date);
         return dateB - dateA;
     });
-
 }
 
 export { checkDate, orderByDateHelper };
